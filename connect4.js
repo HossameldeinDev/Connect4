f357839505c073c47e15e555f67a445bfb6716fb
Hussam Mostafa <hussammostafa@MacBook-Pro.local>
2020-08-07 09:42:15 +0200

    Front end functionality works

--- connect4.js
+++ connect4.js
@@ -1,26 +1,35 @@
-// playerOne = prompt("Player One: Enter your name, You'll be blue")
-// playerTwo = prompt("Player Two: Enter your name, You'll be Red")
-playerOne = "Hussam"
-playerTwo = "Mostafa"
+playerOne = prompt("Player One: Enter your name, You'll be Red")
+playerTwo = prompt("Player Two: Enter your name, You'll be Blue")
+currentPlayer = playerOne
 turnMessage = ": it is your turn, please pick a column to drop your blue chip"
 $('#message').text(playerOne + turnMessage)
 PlayerOneTurn = 1
+colored = [5,5,5,5,5,5,5]
 function colorCell()
 {
-        
+
+
         row = $(this).parent().index()
         column = $(this).index();
-        table = $("table tr");
-        cell = $(table).eq(row).find('td').eq(column)
-        if (PlayerOneTurn === 1)
+        if (colored[column] >= 0)
         {
-            $(cell).css('background', 'red')
-            PlayerOneTurn = 0
+            row = colored[column]
+            colored[column] -=1
+            table = $("table tr");
+            cell = $(table).eq(row).find('td').eq(column)
+            if (PlayerOneTurn === 1)
+            {
+                 $(cell).css('background', 'red')
+                 PlayerOneTurn = 0
+                 currentPlayer = playerTwo
+             }
+            else
+            {
+                 $(cell).css('background', 'blue')
+                 PlayerOneTurn = 1
+                 currentPlayer = playerOne
         }
-        else
-        {
-          $(cell).css('background', 'blue')
-            PlayerOneTurn = 1
         }
+        $('#message').text(currentPlayer + turnMessage)
 }
 $('table td').click(colorCell)
